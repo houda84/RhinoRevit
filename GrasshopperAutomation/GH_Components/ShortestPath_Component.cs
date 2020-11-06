994ae03147c63ef3f8d6dedd4d29644455acd6d7
Omkar Bhagwat <obhagwat29@yahoo.in>
2020-11-06 12:03:06 +0100

    Shortest Path added

---
+++ GrasshopperAutomation/GH_Components/ShortestPath_Component.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using RhinoAutomation.Helper;
+
+namespace GrasshopperAutomation.GH_Components
+{
+    public class ShortestPath_Component : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the ShortestPath_Component class.
+        /// </summary>
+        public ShortestPath_Component()
+          : base("ShortestPath", "shortestPath",
+              "gives the shorts path from start destination to all points",
+              "GrasshopperAutomation", "Lines")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("start point", "startPt", "", GH_ParamAccess.item);
+            pManager.AddPointParameter("target points", "targetPts", "", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddLineParameter("Lines", "lns", "", GH_ParamAccess.list);
+            pManager.AddPointParameter("Points", "pts", "ordered points follows shortes path logic", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Point3d startPt = Point3d.Origin;
+            List<Point3d> points = new List<Point3d>();
+
+            if (!DA.GetData(0, ref startPt)) return;
+            if (!DA.GetDataList(1, points)) return;
+
+            List<Line> lns = HelperClass.MakeShortestPath(startPt, points,out List<Point3d> orderedPts);
+
+            DA.SetDataList(0, lns);
+            DA.SetDataList(1, orderedPts);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("e30791f4-7fc6-479c-b837-71afef2bae17"); }
+        }
+    }
+}
--- RhinoAutomation/Helper/HelperClass.cs
+++ RhinoAutomation/Helper/HelperClass.cs
@@ -5,6 +5,8 @@ using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -72,5 +74,111 @@ namespace RhinoAutomation.Helper
 
             return points;
         }
+
+        public static List<Point3d> GetRandomPoints(int count, int xSize, int ySize, int zSize,out List<Line> lines)
+        {
+            lines = new List<Line>();
+            List<PointInfo> pInfos = PointInfo.GenerateRandomPoints(count*2, xSize,
+                                ySize, zSize);
+            
+
+            List<Point3d> points = new List<Point3d>();
+            foreach (PointInfo p in pInfos)
+            {
+                Point3d pt = new Point3d(p.X, p.Y, p.Z);
+
+                points.Add(pt);
+
+            }
+
+
+            List<Point3d> pts = new List<Point3d>(points);
+
+            Random rand = new Random();
+            
+            while (points.Count >= 2)
+            {
+                count = points.Count;
+
+                int p1Index = rand.Next(0, count);
+                int p2Index = rand.Next(0, count);
+                if (p1Index != p2Index)
+                {
+                    Point3d p1 = points[p1Index];
+                    Point3d p2 = points[p2Index];
+
+                    Line ln = new Line(p1, p2);
+
+                    lines.Add(ln);
+
+                    points.Remove(p1);
+                    points.Remove(p2);
+                }
+
+            }
+
+            return pts;
+        }
+
+        public static List<Line> MakeShortestPath(Point3d startPt,List<Point3d> points,out List<Point3d> orderedPoints)
+        {
+            List<Line> lns = new List<Line>();
+            orderedPoints = new List<Point3d>();
+
+            List<Point3d> pts = new List<Point3d>(points);
+
+
+            Point3d p1 = startPt;
+            //p2 is closest point to p1
+            Point3d p2 = FindClosestPoint(p1, points);
+
+            Line ln = new Line(p1, p2);
+            lns.Add(ln);
+            orderedPoints.Add(p1);
+            orderedPoints.Add(p2);
+
+            while (points.Count >= 2)
+            {
+               
+                points.Remove(p1);
+                points.Remove(p2);
+
+                p1 = p2;
+                p2 = FindClosestPoint(p1, points);
+
+                ln = new Line(p1, p2);
+                lns.Add(ln);
+
+                orderedPoints.Add(p2);
+                //orderedPoints.Add(p2);
+
+
+            }
+            return lns;
+        }
+
+        private static Point3d FindClosestPoint(Point3d pointCloseTo, List<Point3d> pts)
+        {
+            double min = double.MaxValue;
+            Point3d closestPoint = Point3d.Unset;
+
+            List<Point3d> points = new List<Point3d>(pts);
+
+            points.Remove(pointCloseTo);
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Point3d pTest = points[i];
+                double dist = pointCloseTo.DistanceToSquared(pTest);
+                if (dist < min)
+                {
+                    min = dist;
+                    closestPoint = points[i];
+                }
+            }
+
+            return closestPoint;
+        }
+
     }
 }
